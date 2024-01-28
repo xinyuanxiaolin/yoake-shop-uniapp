85eacf7a6a305b716b709667b80af8509ba2d0dd
xinyuanxiaolin <xinyuanxuaolin1@163.com>
2024-01-28 19:37:24 +0800

    实现了弹出层比如送至和服务的弹出和关闭

--- src/pages/goods/goods.vue
+++ src/pages/goods/goods.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
-import { getGoodsByIdAPI } from '@/services/goods';
-import type { GoodsResult } from '@/types/goods';
-import { onLoad } from '@dcloudio/uni-app';
-import { ref } from 'vue';
+import { getGoodsByIdAPI } from '@/services/goods'
+import type { GoodsResult } from '@/types/goods'
+import { onLoad } from '@dcloudio/uni-app'
+import { ref } from 'vue'
+import ServicePanel from './components/ServicePanel.vue'
+import AddressPanel from './components/AddressPanel.vue'
+
 
 /* data */
 // 获取屏幕边界到安全区域距离
@@ -15,28 +18,40 @@ const query = defineProps<{
 const goods = ref<GoodsResult>()
 //当前轮播图页面
 const currentIndex = ref(0)
+//uni-ui弹出层组件ref  这是利用了vue3的组合式API特性,不需要想之前vue2的写法那样
+const popup = ref<{
+  //声明各类型,方便使用
+  open:(type?: UniHelper.UniPopupType)=> void
+  close:() => void
+}>()
+//弹出层条件渲染
+const popupName = ref<'address' | 'service'>()
 
 /* method */
 //获取商品详情数据
-const getGoodsByIdData = async()=>{
+const getGoodsByIdData = async () => {
   const res = await getGoodsByIdAPI(query.id)
   goods.value = res.result
 }
 //轮播图变化
-const onChange:UniHelper.SwiperOnChange = (ev)=>{
-  
-  currentIndex.value  =ev.detail.current
-  
+const onChange: UniHelper.SwiperOnChange = (ev) => {
+  currentIndex.value = ev.detail.current
 }
 //点击图片大图预览
-const onTapImage = (url:string)=>{
+const onTapImage = (url: string) => {
   uni.previewImage({
     current: url,
-    urls:goods.value!.mainPictures
+    urls: goods.value!.mainPictures,
   })
 }
+//弹出层
+const openPopup =(name:typeof popupName.value) =>{
+  //修改弹出层名称
+  popupName.value =name
+  popup.value?.open()
+}
 
-onLoad(()=>{
+onLoad(() => {
   getGoodsByIdData()
 })
 </script>
@@ -48,19 +63,14 @@ onLoad(()=>{
       <!-- 商品主图 -->
       <view class="preview">
         <swiper circular @change="onChange">
-          <swiper-item v-for="item in goods?.mainPictures" :key="item" >
-            <image
-              mode="aspectFill"
-              :src="item"
-              @tap = "onTapImage(item)"
-            />
+          <swiper-item v-for="item in goods?.mainPictures" :key="item">
+            <image mode="aspectFill" :src="item" @tap="onTapImage(item)" />
           </swiper-item>
-          
         </swiper>
         <view class="indicator">
-          <text class="current">{{currentIndex+1}}</text>
+          <text class="current">{{ currentIndex + 1 }}</text>
           <text class="split">/</text>
-          <text class="total">{{goods?.mainPictures.length}}</text>
+          <text class="total">{{ goods?.mainPictures.length }}</text>
         </view>
       </view>
 
@@ -68,10 +78,10 @@ onLoad(()=>{
       <view class="meta">
         <view class="price">
           <text class="symbol">¥</text>
-          <text class="number">{{goods?.price}}</text>
+          <text class="number">{{ goods?.price }}</text>
         </view>
-        <view class="name ellipsis">{{goods?.name}} </view>
-        <view class="desc">{{goods?.desc}} </view>
+        <view class="name ellipsis">{{ goods?.name }} </view>
+        <view class="desc">{{ goods?.desc }} </view>
       </view>
 
       <!-- 操作面板 -->
@@ -80,11 +90,11 @@ onLoad(()=>{
           <text class="label">选择</text>
           <text class="text ellipsis"> 请选择商品规格 </text>
         </view>
-        <view class="item arrow">
+        <view class="item arrow" @tap="openPopup('address')">
           <text class="label">送至</text>
           <text class="text ellipsis"> 请选择收获地址 </text>
         </view>
-        <view class="item arrow">
+        <view class="item arrow" @tap="openPopup('service')">
           <text class="label">服务</text>
           <text class="text ellipsis"> 无忧退 快速退款 免费包邮 </text>
         </view>
@@ -100,13 +110,13 @@ onLoad(()=>{
         <view class="properties">
           <!-- 属性详情 -->
           <view class="item" v-for="item in goods?.details.properties" :key="item.name">
-            <text class="label">{{item.name}}</text>
-            <text class="value">{{item.value}}</text>
+            <text class="label">{{ item.name }}</text>
+            <text class="value">{{ item.value }}</text>
           </view>
         </view>
         <!-- 图片详情 -->
         <image
-          v-for="item in goods?.details.pictures" 
+          v-for="item in goods?.details.pictures"
           :key="item"
           mode="widthFix"
           :src="item"
@@ -127,15 +137,11 @@ onLoad(()=>{
           hover-class="none"
           :url="`/pages/goods/goods?id=${item.id}`"
         >
-          <image
-            class="image"
-            mode="aspectFill"
-            :src="item.picture"
-          ></image>
-          <view class="name ellipsis">{{item.name}}</view>
+          <image class="image" mode="aspectFill" :src="item.picture"></image>
+          <view class="name ellipsis">{{ item.name }}</view>
           <view class="price">
             <text class="symbol">¥</text>
-            <text class="number">{{item.price}}</text>
+            <text class="number">{{ item.price }}</text>
           </view>
         </navigator>
       </view>
@@ -157,6 +163,12 @@ onLoad(()=>{
       <view class="addcart"> 加入购物车 </view>
       <view class="buynow"> 立即购买 </view>
     </view>
+
+    <!-- uni-ui的弹出层使用 -->
+    <uni-popup ref="popup" type="bottom" background-color="#fff">
+      <ServicePanel  v-if="popupName === 'service' " @close="popup?.close()" />
+      <AddressPanel v-if="popupName === 'address' " @close="popup?.close()"/>
+    </uni-popup>
   </view>
 </template>
 
