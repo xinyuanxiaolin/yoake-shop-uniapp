145f84bbbd05bfe93ba2c57b6d32a9fcbb6c62de
xinyuanxiaolin <xinyuanxuaolin1@163.com>
2024-01-31 09:19:51 +0800

    完成购物车底部结算信息

--- src/pages/cart/cart.vue
+++ src/pages/cart/cart.vue
@@ -59,6 +59,34 @@ const onChangeSelectAll =   ()=>{
   //后端数据更新
    putMemberCartSelectApi({selected:_isSelectAll})
 }
+//计算选中的单品列表
+const selectedCartList = computed(()=>{
+  return cartList.value.filter(v=>v.selected)
+})
+//计算选中的总件数
+const selectedCartListCount = computed(()=>{
+  //reduce() 方法对数组中的每个元素按序执行一个提供的 reducer 函数，每一次运行 reducer 会将先前元素的计算结果作为参数传入，最后将其结果汇总为单个返回值。
+  return selectedCartList.value.reduce((sum,item)=>sum+item.count,0)
+})
+//计算选中的总金额
+const selectedCartListMoney = computed(()=>{
+  return selectedCartList.value.reduce((sum,item)=>sum+item.count*item.nowPrice,0)
+})
+//去结算
+const gotoPayment  =()=>{
+  if(selectedCartListCount.value===0){
+   return uni.showToast({
+      icon:'none',
+      title:'请选择商品'
+    })
+  }
+  //跳转到结算页
+  uni.showToast({
+    title: '等待完成',
+    icon: 'none',
+    mask: true
+  })
+}
 //初始化调用
 onShow(() => {
   if (memberStore.profile) {
@@ -131,9 +159,9 @@ onShow(() => {
       <view class="toolbar">
         <text @tap="onChangeSelectAll" class="all" :class="{ checked: isSelectAll }">全选</text>
         <text class="text">合计:</text>
-        <text class="amount">100</text>
+        <text class="amount">{{selectedCartListMoney}}</text>
         <view class="button-grounp">
-          <view class="button payment-button" :class="{ disabled: true }"> 去结算(10) </view>
+          <view @tap="gotoPayment" class="button payment-button" :class="{ disabled: selectedCartListCount===0 }"> 去结算({{ selectedCartListCount }}) </view>
         </view>
       </view>
     </template>
