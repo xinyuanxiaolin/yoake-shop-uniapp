4e4de58ea50792388c3703c3ea1fc42480a7d5f2
xinyuanxiaolin <xinyuanxuaolin1@163.com>
2024-01-31 16:16:11 +0800

    完成订单详情界面骨架屏

---
+++ src/pagesOrder/detail/components/PageSkeleton.vue
@@ -0,0 +1,445 @@
+<template name="skeleton">
+  <view class="sk-container">
+    <view class="navbar navbar" style="padding-top: 44px">
+      <view class="wrap wrap">
+        <navigator
+          class="back icon-left sk-pseudo sk-pseudo-circle"
+          open-type="navigateBack"
+        ></navigator>
+        <view class="title sk-transparent sk-text-14-2857-174 sk-text">订单详情</view>
+      </view>
+    </view>
+    <scroll-view :scroll-y="true" class="viewport viewport" id="scroller" :enable-back-to-top="true">
+      <view class="overview sk-image" style="padding-top: 64px">
+        <view
+          class="status icon-clock sk-transparent sk-text-0-0000-830 sk-text sk-pseudo sk-pseudo-circle"
+          >等待付款</view
+        >
+        <view class="tips sk-transparent">
+          <text class="money sk-transparent sk-text-0-0000-579 sk-text">应付金额: ¥ 99.00</text>
+          <text class="time sk-transparent sk-text-0-0000-932 sk-text">支付剩余</text> 00 时 29 分
+          59 秒
+        </view>
+        <view
+          class="button sk-transparent sk-text-28-1250-313 sk-text"
+          style="background-position-x: 50%"
+          >去支付</view
+        >
+      </view>
+      <view class="shipment shipment">
+        <view class="item sk-image">
+          <view class="message sk-transparent sk-text-14-2857-176 sk-text">
+            您已在广州市天河区黑马程序员完成取件，感谢使用菜鸟驿站，期待再次为您服务。
+          </view>
+          <view class="date sk-transparent sk-text-14-2857-245 sk-text"> 2023-04-14 13:14:20 </view>
+        </view>
+        <view class="locate sk-image">
+          <view class="user sk-transparent sk-text-14-2857-393 sk-text"> 张三 13333333333 </view>
+          <view class="address sk-transparent sk-text-14-2857-516 sk-text">
+            广东省 广州市 天河区 黑马程序员
+          </view>
+        </view>
+      </view>
+      <view class="goods goods">
+        <view class="item item">
+          <navigator class="navigator navigator" hover-class="none">
+            <image class="cover sk-image"></image>
+            <view class="meta meta">
+              <view class="name ellipsis sk-transparent sk-text-14-2857-711 sk-text"
+                >ins风小碎花泡泡袖衬110-160cm</view
+              >
+              <view class="type sk-transparent sk-text-22-2222-313 sk-text">藏青小花， 130</view>
+              <view class="price price">
+                <view class="actual actual">
+                  <text class="symbol sk-transparent sk-opacity">¥</text>
+                  <text class="sk-transparent sk-text-14-2857-505 sk-text">99.00</text>
+                </view>
+              </view>
+              <view class="quantity sk-transparent sk-opacity">x1</view>
+            </view>
+          </navigator>
+          <navigator class="navigator navigator" hover-class="none">
+            <image class="cover sk-image"></image>
+            <view class="meta meta">
+              <view class="name ellipsis sk-transparent sk-text-14-2857-393 sk-text"
+                >ins风小碎花泡泡袖衬110-160cm</view
+              >
+              <view class="type sk-transparent sk-text-22-2222-212 sk-text">藏青小花， 130</view>
+              <view class="price price">
+                <view class="actual actual">
+                  <text class="symbol sk-transparent sk-opacity">¥</text>
+                  <text class="sk-transparent sk-text-14-2857-153 sk-text">99.00</text>
+                </view>
+              </view>
+              <view class="quantity sk-transparent sk-opacity">x1</view>
+            </view>
+          </navigator>
+          <view class="action action">
+            <view
+              class="button primary sk-transparent sk-text-28-3333-314 sk-text"
+              style="background-position-x: 50%"
+              >申请售后</view
+            >
+            <navigator
+              class="button sk-transparent sk-text-28-3333-453 sk-text"
+              style="background-position-x: 50%"
+            >
+              去评价
+            </navigator>
+          </view>
+        </view>
+        <view class="total total">
+          <view class="row row">
+            <view class="text sk-transparent sk-text-0-0000-750 sk-text">商品总价: </view>
+            <view
+              class="symbol sk-transparent sk-text-0-0000-608 sk-text sk-pseudo sk-pseudo-circle"
+              >99.00</view
+            >
+          </view>
+          <view class="row row">
+            <view class="text sk-transparent sk-text-0-0000-890 sk-text">运费: </view>
+            <view
+              class="symbol sk-transparent sk-text-0-0000-193 sk-text sk-pseudo sk-pseudo-circle"
+              >10.00</view
+            >
+          </view>
+          <view class="row row">
+            <view class="text sk-transparent sk-text-0-0000-790 sk-text">应付金额: </view>
+            <view
+              class="symbol primary sk-transparent sk-text-0-0000-315 sk-text sk-pseudo sk-pseudo-circle"
+              >109.00</view
+            >
+          </view>
+        </view>
+      </view>
+      <view class="detail detail">
+        <view class="title sk-transparent sk-text-0-0000-422 sk-text">订单信息</view>
+        <view class="row row">
+          <view class="item sk-transparent">
+            订单编号: 1752605042734534658
+            <text class="copy sk-transparent sk-text-0-0000-560 sk-text">复制</text>
+          </view>
+          <view class="item sk-transparent sk-text-0-0000-125 sk-text"
+            >下单时间: 2023-04-14 13:14:20</view
+          >
+        </view>
+      </view>
+      <view class="toolbar toolbar" style="padding-bottom: 34px">
+        <view class="button primary sk-transparent sk-text-14-2857-585 sk-text"> 去支付 </view>
+        <view class="button sk-transparent sk-text-14-2857-424 sk-text"> 取消订单 </view>
+      </view>
+    </scroll-view>
+  </view>
+</template>
+<style>
+.sk-transparent {
+  color: transparent !important;
+}
+.sk-text-14-2857-174 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 44.8rpx;
+  position: relative !important;
+}
+.sk-text {
+  background-origin: content-box !important;
+  background-clip: content-box !important;
+  background-color: transparent !important;
+  color: transparent !important;
+  background-repeat: repeat-y !important;
+}
+.sk-text-0-0000-830 {
+  background-image: linear-gradient(
+    transparent 0%,
+    #eeeeee 0%,
+    #eeeeee 100%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 36rpx;
+  position: relative !important;
+}
+.sk-text-0-0000-579 {
+  background-image: linear-gradient(
+    transparent 0%,
+    #eeeeee 0%,
+    #eeeeee 100%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 28rpx;
+  position: relative !important;
+}
+.sk-text-0-0000-932 {
+  background-image: linear-gradient(
+    transparent 0%,
+    #eeeeee 0%,
+    #eeeeee 100%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 28rpx;
+  position: relative !important;
+}
+.sk-text-28-1250-313 {
+  background-image: linear-gradient(
+    transparent 28.125%,
+    #eeeeee 0%,
+    #eeeeee 71.875%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 64rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-176 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 36.4rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-245 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 33.6rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-393 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 36.4rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-516 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 33.6rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-711 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 36.4rpx;
+  position: relative !important;
+}
+.sk-text-22-2222-313 {
+  background-image: linear-gradient(
+    transparent 22.2222%,
+    #eeeeee 0%,
+    #eeeeee 77.7778%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 43.2rpx;
+  position: relative !important;
+}
+.sk-opacity {
+  opacity: 0 !important;
+}
+.sk-text-14-2857-505 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 33.6rpx;
+  position: relative !important;
+}
+.sk-text-22-2222-212 {
+  background-image: linear-gradient(
+    transparent 22.2222%,
+    #eeeeee 0%,
+    #eeeeee 77.7778%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 43.2rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-153 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 33.6rpx;
+  position: relative !important;
+}
+.sk-text-28-3333-314 {
+  background-image: linear-gradient(
+    transparent 28.3333%,
+    #eeeeee 0%,
+    #eeeeee 71.6667%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 60rpx;
+  position: relative !important;
+}
+.sk-text-28-3333-453 {
+  background-image: linear-gradient(
+    transparent 28.3333%,
+    #eeeeee 0%,
+    #eeeeee 71.6667%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 60rpx;
+  position: relative !important;
+}
+.sk-text-0-0000-750 {
+  background-image: linear-gradient(
+    transparent 0%,
+    #eeeeee 0%,
+    #eeeeee 100%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 26rpx;
+  position: relative !important;
+}
+.sk-text-0-0000-608 {
+  background-image: linear-gradient(
+    transparent 0%,
+    #eeeeee 0%,
+    #eeeeee 100%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 26rpx;
+  position: relative !important;
+}
+.sk-text-0-0000-890 {
+  background-image: linear-gradient(
+    transparent 0%,
+    #eeeeee 0%,
+    #eeeeee 100%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 26rpx;
+  position: relative !important;
+}
+.sk-text-0-0000-193 {
+  background-image: linear-gradient(
+    transparent 0%,
+    #eeeeee 0%,
+    #eeeeee 100%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 26rpx;
+  position: relative !important;
+}
+.sk-text-0-0000-790 {
+  background-image: linear-gradient(
+    transparent 0%,
+    #eeeeee 0%,
+    #eeeeee 100%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 26rpx;
+  position: relative !important;
+}
+.sk-text-0-0000-315 {
+  background-image: linear-gradient(
+    transparent 0%,
+    #eeeeee 0%,
+    #eeeeee 100%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 36rpx;
+  position: relative !important;
+}
+.sk-text-0-0000-422 {
+  background-image: linear-gradient(
+    transparent 0%,
+    #eeeeee 0%,
+    #eeeeee 100%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 30rpx;
+  position: relative !important;
+}
+.sk-text-0-0000-560 {
+  background-image: linear-gradient(
+    transparent 0%,
+    #eeeeee 0%,
+    #eeeeee 100%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 20rpx;
+  position: relative !important;
+}
+.sk-text-0-0000-125 {
+  background-image: linear-gradient(
+    transparent 0%,
+    #eeeeee 0%,
+    #eeeeee 100%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 26rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-585 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 36.4rpx;
+  position: relative !important;
+}
+.sk-text-14-2857-424 {
+  background-image: linear-gradient(
+    transparent 14.2857%,
+    #eeeeee 0%,
+    #eeeeee 85.7143%,
+    transparent 0%
+  ) !important;
+  background-size: 100% 36.4rpx;
+  position: relative !important;
+}
+.sk-image {
+  background: #efefef !important;
+}
+.sk-pseudo::before,
+.sk-pseudo::after {
+  background: #efefef !important;
+  background-image: none !important;
+  color: transparent !important;
+  border-color: transparent !important;
+}
+.sk-pseudo-rect::before,
+.sk-pseudo-rect::after {
+  border-radius: 0 !important;
+}
+.sk-pseudo-circle::before,
+.sk-pseudo-circle::after {
+  border-radius: 50% !important;
+}
+.sk-container {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  background-color: transparent;
+}
+</style>
